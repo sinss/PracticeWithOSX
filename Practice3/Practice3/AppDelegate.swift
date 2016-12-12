84f6f4fb210853036784061fcb3789bff40df0ce
leo.chang <sinss0000@gmail.com>
2016-12-12 11:27:15 +0800

    Add Practice3

---
+++ Practice3/Practice3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Practice3
+//
+//  Created by Leo on 12/12/2016.
+//  Copyright © 2016 Diing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
